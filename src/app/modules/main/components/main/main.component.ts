e6fc37d56e02f90b76309ea20f439da2cc967d59
Yurii Tsymbala <yurii.tsymbala@devcom.com>
2024-06-02 16:05:45 +0300

    Changed detection strategy for MainComponent

--- src/app/modules/main/components/main/main.component.ts
+++ src/app/modules/main/components/main/main.component.ts
@@ -1,10 +1,9 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
-  styleUrls: ['./main.component.scss']
+  styleUrls: ['./main.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class MainComponent {
-
-}
+export class MainComponent {}
