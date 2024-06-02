1359da12286794d49409bac2fd99383221f71775
Yurii Tsymbala <zumbala2013@gmail.com>
2024-06-02 16:36:43 +0300

    Added Task components

--- src/app/modules/main/main.module.ts
+++ src/app/modules/main/main.module.ts
@@ -2,9 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MainComponent } from './components/main/main.component';
 import { MainRoutingModule } from './main-routing.module';
+import { AddTaskComponent } from './components/tasks/add-task/add-task.component';
+import { TaskListComponent } from './components/tasks/task-list/task-list.component';
+import { TaskComponent } from './components/tasks/task/task.component';
 
 @NgModule({
-  declarations: [MainComponent],
+  declarations: [
+    MainComponent,
+    AddTaskComponent,
+    TaskListComponent,
+    TaskComponent,
+  ],
   imports: [CommonModule, MainRoutingModule],
 })
 export class MainModule {}
