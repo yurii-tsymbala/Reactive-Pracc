f7afba4941789a04c7e8309ad855ce0303425919
Yurii Tsymbala <zumbala2013@gmail.com>
2024-06-02 22:04:38 +0300

    Structured shared module

--- src/app/modules/shared/shared.module.ts
+++ src/app/modules/shared/shared.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { PrimengModule } from './primeng/primeng.module';
+import { PrimengModule } from './modules/primeng/primeng.module';
 
 @NgModule({
   declarations: [],
